311db6d7eade7d34662b9680d44a8aa6024a8f9c
Henrique André <henri.br@gmail.com>
2021-02-21 01:00:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ mkfs.ext4 /dev/sda2
 mkswap /dev/sda1
 swapon /dev/sda1
 mount /dev/sda2 /mnt
+pacman -Syy
+pacman -S reflector
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
@@ -15,3 +18,15 @@ echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo dev > /etc/hostname
 echo -e "127.0.0.1 \t   localhost" >> /etc/hosts
 echo -e "::1        \t  localhost" >> /etc/hosts
+cat /proc/version > /etc/arch-release
+systemctl enable vmware-vmblock-fuse
+systemctl enable vmtoolsd
+Root password
+passwd
+Boot loader
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+Reboot
+exit
+umount -R /mnt
+reboot
