d223d9139fed599462ed066be97f2e74ca1385f9
Henrique André <henri.br@gmail.com>
2021-02-21 12:29:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
-#cat <<- EOF > partitions.txt
-#label: dos
-#label-id: 0xbd531635
-#device: /dev/sda
-#unit: sectors
-#sector-size: 512
 #
-#/dev/sda1 : start=        2048, size=     1048576, type=82
-#/dev/sda2 : start=     1050624, size=   166721536, type=83
-#EOF
+parted -s /dev/sda -- mklabel msdos \
+    mkpart primary linux-swap 1MiB 1.1GiB\
+    mkpart primary ext4 1.1GiB 100% set 1 boot on
+timedatectl set-ntp true
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda1
+swapon /dev/sda1
+mount /dev/sda2 /mnt
+pacman -Syy
+pacman -S --noconfirm --needed --noprogressbar --quiet reflector
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
 
-cat <<- EOF > /mnt/install2.sh
+#
+cat <<- EOF > /mnt/core.sh
 pacman -Syu
 pacman -S  --noconfirm --needed archlinux-keyring
 pacman -S  --noconfirm --needed base-devel linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget
@@ -24,28 +29,14 @@ echo -e "::1        \t  localhost" >> /etc/hosts
 cat /proc/version > /etc/arch-release
 systemctl enable vmware-vmblock-fuse
 systemctl enable vmtoolsd
-echo root:password | chpasswd
+echo root:root | chpasswd
 passwd root -e
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 exit
 EOF
 
-parted -s /dev/sda -- mklabel msdos \
-    mkpart primary linux-swap 1MiB 1.1GiB\
-    mkpart primary ext4 1.1GiB 100% set 1 boot on
-
-timedatectl set-ntp true
-#sfdisk /dev/sda < partitions.txt
-mkfs.ext4 /dev/sda2
-mkswap /dev/sda1
-swapon /dev/sda1
-mount /dev/sda2 /mnt
-pacman -Syy
-pacman -S --noconfirm --needed --noprogressbar --quiet reflector
-reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash -e -x /install2.sh
-umount -R /mnt
+#
+arch-chroot /mnt /bin/bash -e -x /core.sh
+rm /mnt/core.sh
 echo "finished :-) reboot?"
