692db970dd9a19fcc8f743989420e8cf70c8dfbf
Henrique André <henri.br@gmail.com>
2021-02-21 11:41:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,6 @@ exit
 EOF
 
 timedatectl set-ntp true
-curl https://raw.githubusercontent.com/riqueandre/dotfiles/main/partitions.txt -O partitions.txt
 sfdisk /dev/sda < partitions.txt
 mkfs.ext4 /dev/sda2
 mkswap /dev/sda1
