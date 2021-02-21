7865642505383a1b41ee8d63a7811120e4271101
Henrique André <henri.br@gmail.com>
2021-02-21 10:01:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,20 +9,10 @@ sector-size: 512
 /dev/sda2 : start=     1050624, size=   166721536, type=83
 EOF
 
-
-timedatectl set-ntp true
-curl https://raw.githubusercontent.com/riqueandre/dotfiles/main/partitions.txt -O partitions.txt
-sfdisk /dev/sda < partitions.txt
-mkfs.ext4 /dev/sda2
-mkswap /dev/sda1
-swapon /dev/sda1
-mount /dev/sda2 /mnt
-pacman -Syy
-pacman -S reflector
-reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel linux linux-firmware linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget 
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+cat <<- EOF > install2.sh
+pacman -Syu
+pacman -S  --noconfirm --needed archlinux-keyring
+pacman -S  --noconfirm --needed base-devel linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget
 ln -sf /usr/share/zoneinfo/America/Recife /etc/localtime
 hwclock --systohc
 sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
@@ -40,3 +30,18 @@ grub-mkconfig -o /boot/grub/grub.cfg
 exit
 umount -R /mnt
 reboot
+EOF
+
+timedatectl set-ntp true
+curl https://raw.githubusercontent.com/riqueandre/dotfiles/main/partitions.txt -O partitions.txt
+sfdisk /dev/sda < partitions.txt
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda1
+swapon /dev/sda1
+mount /dev/sda2 /mnt
+pacman -Syy
+pacman -S reflector
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash -e -x /second.sh
