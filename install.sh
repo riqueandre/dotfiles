320da092835ccdeacfda23cea053849b90b0b67a
Henrique André <henri.br@gmail.com>
2021-02-21 15:49:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,56 @@
 #
+timedatectl set-ntp true
+
 parted -s /dev/sda -- mklabel msdos \
     mkpart primary linux-swap 1MiB 1.1GiB\
     mkpart primary ext4 1.1GiB -1 \ 
     set 2 boot on
-timedatectl set-ntp true
+    
 mkfs.ext4 /dev/sda2
 mkswap /dev/sda1
+
 swapon /dev/sda1
 mount /dev/sda2 /mnt
+
 pacman -Syy
 pacman -S --noconfirm --needed --noprogressbar --quiet reflector
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-firmware
+
 genfstab -U /mnt >> /mnt/etc/fstab
 
 #
 cat <<- EOF > /mnt/core.sh
 pacman -Syu
 pacman -S  --noconfirm --needed archlinux-keyring
-pacman -S  --noconfirm --needed base-devel linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget man openssh 
+pacman -S  --noconfirm --needed base-devel linux-headers asp vi vim nano dhcpcd grub sudo open-vm-tools gtkmm gtkmm3 git wget man openssh
+
 ln -sf /usr/share/zoneinfo/America/Recife /etc/localtime
 hwclock --systohc
+
 sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+
 echo dev > /etc/hostname
 echo -e "127.0.0.1 \t   localhost" >> /etc/hosts
 echo -e "::1        \t  localhost" >> /etc/hosts
+
 cat /proc/version > /etc/arch-release
-systemctl enable vmware-vmblock-fuse
-systemctl enable vmtoolsd
-echo root:root | chpasswd
+
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+
 systemctl enable dhcpcd
 systemctl enable vmware-vmblock-fuse
 systemctl enable vmtoolsd
+
+echo root:root | chpasswd
+
 exit
 EOF
 
 #
 arch-chroot /mnt /bin/bash -e -x /core.sh
 rm /mnt/core.sh
-echo "finished :-) reboot?"
+echo "finished :-) umount -R /mnt and reboot?"
