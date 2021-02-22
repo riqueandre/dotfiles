58cf2ee4c33359211d08359e8c9c103dfec8d61e
Henrique André <henri.br@gmail.com>
2021-02-21 23:19:37 -0300

    Update xmonad_install.sh

--- xmonad_install.sh
+++ xmonad_install.sh
@@ -1,4 +1,4 @@
-pacman -S --noconfirm --needed xorg-server xf86-input-vmmouse xf86-video-vmware mesa
+pacman -S --noconfirm --needed xorg-server xf86-input-vmmouse xf86-video-vmware mesa xorg-xinput xorg-xinit
 
 pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
@@ -7,6 +7,7 @@ makepkg -si
 
 yay --noconfirm --needed -Syu
 yay --noconfirm --needed -S ly
+yay -S acpi alacritty xmonad-contrib xclip
 
 pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar
 
