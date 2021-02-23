5873c0d552d6bf328e455446d96286fcbbbc1126
Henrique André <henri.br@gmail.com>
2021-02-23 13:21:32 -0300

    Update i3_install.sh

--- i3_install.sh
+++ i3_install.sh
@@ -1,4 +1,3 @@
 pacman -S --noconfirm --needed xorg-server xf86-input-vmmouse xf86-video-vmware mesa xorg-xinput xorg-xinit
-pacman -S i3-gaps i3status i3blocks
-
-yay -S alacritty
+pacman -S --noconfirm --needed i3-gaps i3status i3blocks
+pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
