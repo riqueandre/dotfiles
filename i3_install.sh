eee66f3c4287d26b4ac71d685dea0fa397b342f1
Henrique André <henri.br@gmail.com>
2021-02-23 13:35:36 -0300

    Update i3_install.sh

--- i3_install.sh
+++ i3_install.sh
@@ -1,5 +1,6 @@
 pacman -S --noconfirm --needed xorg-server xf86-input-vmmouse xf86-video-vmware mesa xorg-xinput xorg-xinit
-pacman -S --noconfirm --needed i3-gaps i3status i3blocks
 pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
+pacman -S --noconfirm --needed i3-gaps i3status i3blocks
+pacman -S --noconfirm --needed rofi picom xnumlock imwheel
 
 systemctl enable lightdm
