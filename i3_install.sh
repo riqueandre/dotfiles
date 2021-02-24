a6c99bfbe205e027deb9631abc894017a074b44d
Henrique André <henri.br@gmail.com>
2021-02-23 23:28:53 -0300

    Update i3_install.sh

--- i3_install.sh
+++ i3_install.sh
@@ -1,6 +1,11 @@
-pacman -S --noconfirm --needed xorg-server xf86-input-vmmouse xf86-video-vmware mesa xorg-xinput xorg-xinit
-pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
-pacman -S --noconfirm --needed i3-gaps i3status i3blocks
-pacman -S --noconfirm --needed rofi picom xnumlock imwheel
+sudo pacman -S --noconfirm --needed xorg-server xorg-xinput xorg-xinit xorg-xfd xorg-xrdb mesa xf86-input-vmmouse xf86-video-vmware
+sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
+sudo pacman -S --noconfirm --needed i3status i3blocks
+sudo pacman -S --noconfirm --needed rofi picom xnumlock imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen polybar pfetch chromium
+sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts
+
+yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete
 
 systemctl enable lightdm
+
+
