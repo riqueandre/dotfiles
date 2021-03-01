cc333b86284761c96bbc5f073ccf0c31b2449daa
Henrique André <henri.br@gmail.com>
2021-03-01 16:51:39 -0300

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -2,9 +2,9 @@ sudo pacman -S --noconfirm --needed xorg-server xorg-xinput xorg-xinit xorg-xfd 
 sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
 sudo pacman -S --noconfirm --needed i3status i3blocks
 sudo pacman -S --noconfirm --needed rofi picom xnumlock imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen polybar pfetch chromium network-manager-applet networkmanager-openvpn unzip python-pywal calc mpd volumeicon xfce4-power-manager polkit-gnome
-sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts terminus-font nemo gnome-font-viewer
+sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts terminus-font nemo gnome-font-viewer ttf-fantasque-sans-mono
 
-yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete networkmanager-dmenu-gi siji-gitt ttf-material-design-icons-git numlockx
+yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete networkmanager-dmenu-gi siji-gitt ttf-material-design-icons-git numlockx ttf-icomoon-feather
 
 systemctl enable lightdm
 systemctl enable mpd
