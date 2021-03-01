b8c1a4ab0f2e0fb8ea0d10e49d1c5a07931e4f7d
Henrique André <henri.br@gmail.com>
2021-03-01 16:36:17 -0300

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,11 +1,13 @@
 sudo pacman -S --noconfirm --needed xorg-server xorg-xinput xorg-xinit xorg-xfd xorg-xrdb xorg-xsetroot mesa xf86-input-vmmouse xf86-video-vmware
 sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
 sudo pacman -S --noconfirm --needed i3status i3blocks
-sudo pacman -S --noconfirm --needed rofi picom xnumlock imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen polybar pfetch chromium network-manager-applet networkmanager-openvpn unzip python-pywal calc
-sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts
+sudo pacman -S --noconfirm --needed rofi picom xnumlock imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen polybar pfetch chromium network-manager-applet networkmanager-openvpn unzip python-pywal calc mpd volumeicon xfce4-power-manager polkit-gnome
+sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts terminus-font
 
-yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete networkmanager-dmenu-git
+yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete networkmanager-dmenu-gi siji-gitt ttf-material-design-icons-git numlockx
 
 systemctl enable lightdm
+systemctl enable mpd
+systemctl enable NetworkManager
 
 #homectl update --shell=$(which zsh) henrique
