418130f3399c38291e1a730802d5af7d1b939937
Henrique André <henri.br@gmail.com>
2021-03-01 23:18:26 -0300

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,7 +1,7 @@
 sudo pacman -S --noconfirm --needed xorg-server xorg-xinput xorg-xinit xorg-xfd xorg-xrdb xorg-xsetroot mesa xf86-input-vmmouse xf86-video-vmware
 sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
 sudo pacman -S --noconfirm --needed i3status i3blocks
-sudo pacman -S --noconfirm --needed rofi picom xnumlock imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen chromium networkmanager network-manager-applet networkmanager-openvpn unzip python-pywal calc mpd volumeicon xfce4-power-manager polkit-gnome
+sudo pacman -S --noconfirm --needed rofi picom imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen chromium networkmanager network-manager-applet networkmanager-openvpn unzip python-pywal calc mpd volumeicon xfce4-power-manager polkit-gnome
 sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts terminus-font nemo gnome-font-viewer ttf-fantasque-sans-mono
 
 git clone https://aur.archlinux.org/yay-git.git
