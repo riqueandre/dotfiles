b395a7a49d3bf4ce8589b29e8d06178cb147340e
Henrique André <henri.br@gmail.com>
2021-03-06 22:38:13 -0300

    Update archlinux_postinstall.sh

--- archlinux_postinstall.sh
+++ archlinux_postinstall.sh
@@ -1,23 +1,89 @@
 #!/bin/bash
-set -e
 
 mkdir -p .config
 mkdir -p .local/share
 
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
-cd ~
+if [[ -d $HOME/yay-git ]]
+then
+	echo "skip yay install"
+else
+	git clone https://aur.archlinux.org/yay-git.git
+	cd yay-git
+	makepkg -si
+	cd ~
+fi
 
-git clone https://github.com/riqueandre/dotfiles
 
-sudo pacman -S --noconfirm --needed xorg-server xorg-xinput xorg-xinit xorg-xfd xorg-xrdb xorg-xsetroot hsetroot mesa xf86-input-vmmouse xf86-video-vmware
-sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
-sudo pacman -S --noconfirm --needed i3status i3blocks
-sudo pacman -S --noconfirm --needed rofi imwheel alsa-utils pulseaudio paprefs pavucontrol pulseaudio-alsa nitrogen chromium networkmanager network-manager-applet networkmanager-openvpn unzip python-pywal calc mpd volumeicon xfce4-power-manager polkit-gnome
-sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family noto-fonts gsfonts terminus-font nemo gnome-font-viewer ttf-fantasque-sans-mono
+if [[ -d $HOME/dotfiles ]]
+then
+	echo "skip clone dotfiles"
+else
+	git clone https://github.com/riqueandre/dotfiles
+fi
 
-yay -S i3-gaps alacritty autotiling polybar pfetch i3exit nerd-fonts-complete networkmanager-dmenu-git siji-git ttf-material-design-icons-git numlockx ttf-icomoon-feather picom-ibhagwan-git i3-scrot nvim
+
+pacman_pkgs=(xorg-server \
+	xorg-xinput xorg-xinit \
+	xorg-xfd \
+	xorg-xrdb \
+	xorg-xsetroot \
+	hsetroot \
+	mesa \
+	xf86-input-vmmouse \
+	xf86-video-vmware \
+	lightdm lightdm-gtk-greeter \
+	i3status \
+	i3blocks \
+	rofi \
+	imwheel \
+	alsa-utils \
+	pulseaudio \
+	paprefs \
+	pavucontrol \
+	pulseaudio-alsa \
+	nitrogen \
+	chromium  \
+	networkmanager \
+	network-manager-applet \
+	networkmanager-openvpn \
+	unzip \
+	python-pywal \
+	calc \
+	mpd \
+	volumeicon \
+	xfce4-power-manager \
+	polkit-gnome \
+	neovim  \
+	ttf-ubuntu-font-family \
+	noto-fonts \
+	gsfonts \
+	terminus-font \
+	nemo \
+	gnome-font-viewer \
+	ttf-fantasque-sans-mono)
+for pkg in "${pacman_pkgs[@]}"
+do
+	sudo pacman -S --noconfirm --needed $pkg 2>> archlinux_postinstall.log
+done
+
+
+yay_pkgs=(i3-gaps \
+	alacritty \
+	autotiling \
+	polybar \
+	pfetch \
+	i3exit \
+	nerd-fonts-complete \
+	networkmanager-dmenu-git \
+	siji-git \
+	picom-ibhagwan-git \
+	i3-scrot)
+
+for pkg in "${yay_pkgs[@]}"
+do
+	yay -S $pkg 2>> archlinux_postinstall.log
+done
+
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -33,4 +99,4 @@ sudo systemctl enable lightdm
 sudo systemctl enable mpd
 sudo systemctl enable NetworkManager
 
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
