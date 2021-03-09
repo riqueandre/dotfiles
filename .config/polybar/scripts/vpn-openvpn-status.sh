1bb7c56f42829facb2fe1bdca8129e595f89d09c
Henrique André <henri.br@gmail.com>
2021-03-09 02:20:11 -0300

    update

--- .config/polybar/scripts/vpn-openvpn-status.sh
+++ .config/polybar/scripts/vpn-openvpn-status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo down) | head -n 1
+printf "VPN: " && (nmcli | grep VPN | head -n 1 | awk '{print $1}' | cut -d '.' -f 1 && echo down) | head -n 1
