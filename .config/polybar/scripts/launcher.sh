e2661d892b9874a372a9525e0069aca3e6700f2c
Henrique André <henri.br@gmail.com>
2021-03-12 17:48:07 -0300

    update

--- .config/polybar/scripts/launcher.sh
+++ .config/polybar/scripts/launcher.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-local style=$POLYBAR_STYLE
-
+ACTIVE_STYLE=$POLYBAR_STYLE
 # arguments supplied
 if [ $# -ne 0 ]
 then
-    style=$1
+    ACTIVE_STYLE=$1
 fi
-
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/$style/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/$ACTIVE_STYLE/rofi/launcher.rasi
