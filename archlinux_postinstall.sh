048df957bb8d09e51e1afd3669fc96b5b142a3b0
Henrique André <henri.br@gmail.com>
2021-03-25 19:06:36 -0300

    update

--- archlinux_postinstall.sh
+++ archlinux_postinstall.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+case $1 in
+    i3)
+        wm_selected="i3status i3blocks i3-gaps i3exit i3-scrot autotiling"
+    ;;
+
+    bspwm)
+        wm_selected="bspwm sxhkd"
+    ;;
+
+    *)
+        echo "Select bspwm or i3"
+        exit 0
+    ;;
+esac
+
 mkdir -p .config
 mkdir -p .local/share
+
 
 if [[ -d $HOME/pamac-aur ]]
 then
@@ -14,6 +30,7 @@ else
     cd ~
 fi
 
+
 if [[ -d $HOME/dotfiles ]]
 then
     echo "skipping clone dotfiles"
@@ -21,89 +38,116 @@ else
     git clone https://github.com/riqueandre/dotfiles
 fi
 
-case $1 in
-  i3)
-    wm="i3status i3blocks i3-gaps i3exit i3-scrot"
-    ;;
 
-  bspwm)
-    wm="bspwm sxhkd"
-    ;;
-esac
-
-pkgs="
-    $wm \
-    alacritty \
-    alsa-utils \
-    autotiling \
-    bat \
-    calc \
-    cava \
-    conky \
-    dunst \
-    exo \
-    feh \
-    gnome-font-viewer \
-    google-chrome \
-    gsfonts \
-    hsetroot \
-    imwheel \
-    lightdm lightdm-gtk-greeter \
-    mesa \
-    mpd \
-    ncmpcpp \
-    nemo \
-    neovim  \
-    nerd-fonts-complete \
-    network-manager-applet \
-    networkmanager-dmenu-git \
-    noto-fonts \
-    numlockx \
-    papirus-icon-theme \
-    paprefs \
-    pavucontrol \
-    pfetch \
-    picom-ibhagwan-git \
-    polkit-gnome \
-    polybar \
-    pulseaudio \
-    pulseaudio-alsa \
-    python-pip \
-    python-pip \
-    python-pywal \
-    rofi \
-    rofi-dmenu \
-    siji-git \
-    terminus-font \
-    ttf-fantasque-sans-mono
-    ttf-sarasa-gothic \
-    ttf-ubuntu-font-family \
-    tty-clock \
-    unzip \
-    volumeicon \
-    xdg-user-dirs \
-    xf86-input-vmmouse \
-    xf86-video-vmware \
-    xfce4-power-manager \
-    xorg-server \
-    xorg-xfd \
-    xorg-xinput xorg-xinit \
-    xorg-xrdb \
-    xorg-xsetroot \
-    xss-lock \
+pkgs_video="
+    mesa 
+    xf86-input-vmmouse 
+    xf86-video-vmware 
+    xfce4-power-manager 
+    xorg-server 
+    xorg-xfd 
+    xorg-xinput xorg-xinit 
+    xorg-xrdb 
+    xorg-xsetroot 
     "
 
+pkgs_dm="
+    lightdm 
+    lightdm-gtk-greeter 
+    "
+
+
+pkgs_wm="
+    ${wm_selected} 
+    picom-ibhagwan-git 
+    dunst 
+    rofi 
+    rofi-dmenu 
+    polybar 
+    "
+
+pkgs_audio="
+    alsa-utils 
+    volumeicon 
+    pavucontrol
+    paprefs
+    cava
+    pulseaudio
+    pulseaudio-alsa
+    "
+
+pkgs_network="
+    network-manager-applet
+    networkmanager-dmenu-git
+    "
+
+
+pkgs_util="
+    numlockx
+    bat
+    calc
+    feh
+    gnome-font-viewer
+    nemo
+    neovim
+    alacritty
+    unzip
+    xdg-user-dirs
+    xss-lock 
+    conky
+    exo
+    google-chrome
+    hsetroot
+    imwheel
+    mpd
+    ncmpcpp
+    papirus-icon-theme
+    pfetch
+    polkit-gnome
+    "
+
+
+pkgs_fonts="
+    siji-git
+    terminus-font
+    ttf-fantasque-sans-mono
+    ttf-sarasa-gothic
+    ttf-ubuntu-font-family
+    nerd-fonts-complete
+    noto-fonts
+    gsfonts
+    "
+
+
+pkgs_dev="
+    python-pip
+    openjdk11-doc
+    openjdk11-src
+    "
+
+
+pkgs="
+    ${pkgs_video}   
+    ${pkgs_dm}      
+    ${pkgs_wm}      
+    ${pkgs_audio}   
+    ${pkgs_network} 
+    ${pkgs_util}    
+    ${pkgs_fonts}   
+    ${pkgs_dev}
+    "
 sudo pamac install --no-confirm $pkgs
 
 
 if [[ -d $HOME/.oh-my-zsh ]]
 then
-    echo "skipping oh-my-zsh installation"
+    echo "skipping oh-my-zsh and pluggins installation"
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
+
 
 if [[ -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]]
 then
@@ -119,11 +163,22 @@ cp -r ~/dotfiles/.zshrc                             ~/
 cp -r ~/dotfiles/.zshenv                            ~/
 cp -r ~/dotfiles/.config                            ~/
 cp -r ~/dotfiles/.local/                            ~/
-cp -r ~/dotfiles/themes/bspwm-b4skyx/.Xresources    ~/
-cp -r ~/dotfiles/themes/bspwm-b4skyx/polybar        ~/.config/
-cp -r ~/dotfiles/themes/bspwm-b4skyx/picom          ~/.config/
-cp -r ~/dotfiles/themes/bspwm-b4skyx/dunst          ~/.config/
-cp -r ~/dotfiles/themes/bspwm-b4skyx/rofi           ~/.config/
+case $1 in
+  i3)
+    cp -r ~/dotfiles/themes/i3-cuts/.Xresources    ~/
+    cp -r ~/dotfiles/themes/i3-cuts/polybar        ~/.config/
+    cp -r ~/dotfiles/themes/i3-cuts/picom          ~/.config/
+    cp -r ~/dotfiles/themes/i3-cuts/dunst          ~/.config/
+    cp -r ~/dotfiles/themes/i3-cuts/rofi           ~/.config/      
+    ;;
+  bspwm)
+    cp -r ~/dotfiles/themes/bspwm-b4skyx/.Xresources    ~/
+    cp -r ~/dotfiles/themes/bspwm-b4skyx/polybar        ~/.config/
+    cp -r ~/dotfiles/themes/bspwm-b4skyx/picom          ~/.config/
+    cp -r ~/dotfiles/themes/bspwm-b4skyx/dunst          ~/.config/
+    cp -r ~/dotfiles/themes/bspwm-b4skyx/rofi           ~/.config/
+    ;;
+esac
 
 
 sudo systemctl enable lightdm
